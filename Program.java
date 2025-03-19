cc01a7bf5ee3ed220c51e942d44779a27f26a7b2
Junior-hernandez <jhernandezg92@miumg.edu.gt>
2025-03-18 23:55:19 -0600

    experimental commit

---
+++ Program.java
@@ -0,0 +1,9 @@
+package Grupo5PrograIB2025;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Program{
+    public static void main(String[] args){
+        
+    }
+}
--- s.java
+++
@@ -1,9 +0,0 @@
-package Grupo5PrograIB2025;
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
-public class s{
-    public static void main(String[] args){
-        
-    }
-}
