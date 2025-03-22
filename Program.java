b3fcbb5123dd8a3297f1c906293099bc6b53496e
Junior-hernandez <jhernandezg92@miumg.edu.gt>
2025-03-21 19:17:51 -0600

    eliminar archivo duplicado

--- Program.java
+++
@@ -1,9 +0,0 @@
-package Grupo5PrograIB2025;
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
-public class Program{
-    public static void main(String[] args){
-        
-    }
-}
