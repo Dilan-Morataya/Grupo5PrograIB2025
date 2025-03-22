64934f53a1f041e899f095ac2556cdcbb127f1f3
Junior-hernandez <jhernandezg92@miumg.edu.gt>
2025-03-22 14:28:13 -0600

    Agregar función para leer Csv

--- Program.java
+++ Program.java
@@ -1,9 +1,33 @@
 package Grupo5PrograIB2025;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Program{
+    public static class ManejoDeCsv{
+
+        public static void leerCsv(String filepath){
+            String line;
+
+            try(BufferedReader BuffR = new BufferedReader(new FileReader(filepath))){
+
+                while((line=BuffR.readLine())!=null){
+                    String[] values=line.split(",");
+                    for(String value : values){
+                        System.out.println(value+" ");
+                    }
+                    System.out.println();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+                System.out.println("\"Error pues\"");
+            }
+        }
+    }
     public static void main(String[] args){
+        ManejoDeCsv.leerCsv("Archivo.csv");
         
     }
 }
