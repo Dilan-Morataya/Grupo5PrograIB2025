05c46f7d1e9a93e80142ea2f8bd715664d7a22f6
dilan morataya <dmoratayah@miumg.edu.gt>
2025-03-23 14:09:00 -0600

    feature: integrar menú al programa

--- Program.java
+++ Program.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program{
- 
+    static Scanner sc = new Scanner(System.in);
         abstract static class Animal {
     
             protected String nombre;
@@ -64,7 +64,7 @@ public class Program{
             }
     
         }
-    
+        // fin
     
     
         static class Mamifero extends Animal {
@@ -171,9 +171,28 @@ public class Program{
     public static class ManejoDeCsv{
 
         // funcion para escribir en archivos csv (hace falta añadir el nombre de las clases para guardarlas cuando las haya)
-        public static void escribirCsv(Animal animalxd){
+       
+        public static void escribirCsvReptil(Reptil reptilxd){
             try(BufferedWriter BuffW=new BufferedWriter(new FileWriter("registro.csv", true))){ 
-                String line = animalxd.nombre+","+animalxd.edad+","+animalxd.peso+","+animalxd.dieta;
+                String line = reptilxd.nombre+","+reptilxd.edad+","+reptilxd.peso+","+reptilxd.dieta;
+                BuffW.write(line);
+                BuffW.newLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        public static void escribirCsvAve(Ave avexd){
+            try(BufferedWriter BuffW=new BufferedWriter(new FileWriter("registro.csv", true))){ 
+                String line = avexd.nombre+","+avexd.edad+","+avexd.peso+","+avexd.dieta;
+                BuffW.write(line);
+                BuffW.newLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        public static void escribirCsvMamifero(Mamifero mamiferoxd){
+            try(BufferedWriter BuffW=new BufferedWriter(new FileWriter("registro.csv", true))){ 
+                String line = mamiferoxd.nombre+","+mamiferoxd.edad+","+mamiferoxd.peso+","+mamiferoxd.dieta;
                 BuffW.write(line);
                 BuffW.newLine();
             }catch(IOException e){
@@ -202,22 +221,19 @@ public class Program{
 
 
     public static void menu(){
-        Scanner sc = new Scanner(System.in);
-            System.out.print("ingrese su opcion:");
-        int eleccion= sc.nextInt();
         do{
            System.out.println(" Menu Principal ");
            System.out.println("1.Zoo");
            System.out.println("2. Fase II");
            System.out.println("3. Fase III");
            System.out.println("4. Salir");
-           System.out.println("Ingrese un numero para elegir una Opcion");
-           
-           eleccion = sc.nextInt();
+           System.out.print("ingrese su opcion:");
+
+           int eleccion= sc.nextInt();
            
            switch (eleccion) {
                case 1:
-                   menuZoo(sc);
+                   menuZoo();
                    break;
                case 2:
                    System.out.println("FASE II");
@@ -228,20 +244,67 @@ public class Program{
                case 4:
                    System.out.println("Saliendo del programa");
                    System.out.println("Adios, vuelve pronto");
-                   break;
+                   return;
                default:
                    System.out.println("opcion no valida, selecciona un numero del 1 al 4");
                              }
            
-           }while (eleccion !=4);
+           }while (true);
         
       
         }
     
-    public static void menuZoo(Scanner sc) {
-        int opcion;
-        // Archivo ob = new Archivo();
-        // Leer le = new Leer();
+        public static void agregarAnimal(){
+            System.out.println("elija el tipo de animal que quiere agregar: "+"\n"+"1. mamifero"+"\n"+"2. ave"+"\n"+"3. eptil");
+            int tipoAnimal=sc.nextInt();
+            switch(tipoAnimal){
+                case 1:
+                    System.out.print("ingrese el nombre del mamifero: ");
+                    sc.nextLine();
+                    String nombreMamifero=sc.nextLine();
+                    System.out.print("ingrese la edad de mamifero: ");
+                    int edadMamifero=sc.nextInt();
+                    System.out.print("ingrese el peso del mamifero: ");
+                    double pesoMamifero=sc.nextDouble();
+                    System.out.print("ingrese la dieta del mamifero: ");
+                    String dietaMamifero=sc.nextLine();
+                    sc.nextLine();
+                    Mamifero mamifero =new Mamifero(nombreMamifero,edadMamifero,pesoMamifero,dietaMamifero);
+                    ManejoDeCsv.escribirCsvMamifero(mamifero);
+                   
+                    break;
+                case 2:
+                    System.out.print("ingrese el nombre del ave: ");
+                    sc.nextLine();
+                    String nombreAve=sc.nextLine();
+                    System.out.print("ingrese la edad del ave: ");
+                    int edadAve=sc.nextInt();
+                    System.out.print("ingrese el peso del ave: ");
+                    double pesoAve=sc.nextDouble();
+                    System.out.print("ingrese la dieta del ave: ");
+                    String dietaAve=sc.nextLine();
+                    sc.nextLine();
+                    Ave ave =new Ave(nombreAve,edadAve,pesoAve,dietaAve);
+                    ManejoDeCsv.escribirCsvAve(ave);
+                    break;
+                case 3:
+                    System.out.print("ingrese el nombre del reptil: ");
+                    sc.nextLine();
+                    String nombreReptil=sc.nextLine();
+                    System.out.print("ingrese la edad del reptil: ");
+                    int edadReptil=sc.nextInt();
+                    System.out.print("ingrese el peso del reptil: ");
+                    double pesoReptil=sc.nextDouble();
+                    System.out.print("ingrese la dieta del reptil: ");
+                    String dietaReptil=sc.nextLine();
+                    sc.nextLine();
+                    Reptil reptil =new Reptil(nombreReptil,edadReptil,pesoReptil,dietaReptil);
+                    ManejoDeCsv.escribirCsvReptil(reptil);
+                    break;
+            }
+        }
+
+    public static void menuZoo() {
         do {
             System.out.println("\n Menú Zoo ");
             System.out.println("1. Agregar nuevo animal y cantidad de comida que come");
@@ -249,22 +312,17 @@ public class Program{
             System.out.println("3. Exportar datos a CSV");
             System.out.println("4. Volver al menú principal");
             System.out.print("Ingrese un numero para elegir una Opcion");
-            opcion = sc.nextInt();
+            // sc.nextInt();
+            int opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1:
                     System.out.println("Opción 1: Agregar nuevo animal y la cantidad de comida que come.");
-                    //   Archivo.abrirArchivo();
-                    //   Archivo.agregarRegistros();
-                    //   Archivo.cerrarArchivos();
-                    
+                    agregarAnimal();
                     break;
                 case 2:
                     System.out.println("Opción 2: Mostrar todos los animales del Zoo.");
                     ManejoDeCsv.leerCsv("registro.csv");
-                    //   Leer.abrirArchivo();
-                    //   Leer.leerRegistros();
-                    //   Leer.cerrarArchivo();
                     break;
                 case 3:
                     System.out.println("Opción 3: Exportar datos a CSV.");
@@ -273,17 +331,15 @@ public class Program{
                     break;
                 case 4:
                     System.out.println("Volviendo al menú principal...");
-                    break;
+                    return;
                 default:
                     System.out.println("Opción inválida, por favor seleccione entre 1 y 4.");
             }
-        } while (opcion != 4);
+        } while (true);
     }
 
 
     public static void main(String[] args){
-        Ave avexd=new Ave("alcon",2,2,"pollitos");
-        ManejoDeCsv.escribirCsv(avexd);
         menu();
     }
 }
