4bd87b6d537a17f53da51ea4c17c23c2a430d935
Junior-hernandez <jhernandezg92@miumg.edu.gt>
2025-03-22 23:23:03 -0600

    añadidas clases

--- Program.java
+++ Program.java
@@ -8,7 +8,164 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program{
+ 
+        abstract static class Animal {
+    
+            protected String nombre;
+    
+            protected int edad;
+    
+            protected double peso;
+    
+            protected String dieta;
+    
+    
+    
+            public Animal(String nombre, int edad, double peso, String dieta) {
+    
+                this.nombre = nombre;
+    
+                this.edad = edad;
+    
+                this.peso = peso;
+    
+                this.dieta = dieta;
+    
+            }
+    
+    
+    
+            public abstract void alimentar();
+    
+    
+    
+            public double calcularAlimento(int días) {
+    
+                if (días == 0) {
+    
+                    return 0;
+    
+                }
+    
+                return consumoDiario() + calcularAlimento(días - 1);
+    
+            }
+    
+    
+    
+            protected abstract double consumoDiario();   
+    
+            @Override
+    
+            public String toString() {
+    
+                return nombre + " - " + edad + " años - " + peso + " kg - " + dieta;
+    
+            }
+    
+        }
+    
+    
+    
+        static class Mamifero extends Animal {
+    
+            public Mamifero(String nombre, int edad, double peso, String dieta) {
+    
+                super(nombre, edad, peso, dieta);
+    
+            }
+    
+    
+    
+            @Override
+    
+            public void alimentar() {
+    
+                System.out.println(nombre + " (mamífero) ha sido alimentado.");
+    
+            }
+    
+    
+    
+            @Override
+    
+            public double consumoDiario() {
+    
+                return 10; // Ejemplo: 10 kg de alimento al día
+    
+            }
+    
+        }
+    
+    
+    
+        static class Ave extends Animal {
+    
+            public Ave(String nombre, int edad, double peso, String dieta) {
+    
+                super(nombre, edad, peso, dieta);
+    
+            }
+    
+    
+    
+            @Override
+    
+            public void alimentar() {
+    
+                System.out.println(nombre + " (ave) ha sido alimentado.");
+    
+            }
+    
+    
+    
+            @Override
+    
+            public double consumoDiario() {
+    
+                return 2; // Ejemplo: 2 kg de alimento al día
+    
+            }
+    
+        }
+    
+    
+    
+        static class Reptil extends Animal {
+    
+            public Reptil(String nombre, int edad, double peso, String dieta) {
+    
+                super(nombre, edad, peso, dieta);
+    
+            }
+    
+    
+    
+            @Override
+    
+            public void alimentar() {
+    
+                System.out.println(nombre + " (reptil) ha sido alimentado.");
+    
+            }
+    
+    
+    
+            @Override
+    
+            public double consumoDiario() {
+    
+                return 5; // Ejemplo: 5 kg de alimento al día
+    
+            }
+    
+        } 
 
+
+
+
+
+        
     //  --clase para manejar archivos CSV (aún en desarrollo*)-.
     // @autor Junior-Hernandez 
     public static class ManejoDeCsv{
