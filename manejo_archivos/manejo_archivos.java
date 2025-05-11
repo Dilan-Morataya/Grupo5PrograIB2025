dfb1a541e7994a9653b5350c6c13680059890755
Dilan-Morataya <dmoratayah@miumg.edu.gt>
2025-05-11 01:06:25 -0600

    Update manejo_archivos.java

--- manejo_archivos/manejo_archivos.java
+++ manejo_archivos/manejo_archivos.java
@@ -1,5 +1,28 @@
 package manejo_archivos;
 
 public class manejo_archivos {
-    
+    import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+class ArchivoCSV {
+    public static void exportar(List<Animal> animales) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("animales.csv"))) {
+            for (Animal a : animales) {
+                writer.write(a.getTipo() + "," + a.getNombre() + "," + a.especie + "," + a.edad + "," + a.cantidadComidaDiaria);
+                writer.newLine();
+            }
+            System.out.println("Datos exportados exitosamente a animales.csv");
+        } catch (IOException e) {
+            System.out.println("Error al exportar archivo CSV: " + e.getMessage());
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        zoo.menu();
+    }
+}
 }
